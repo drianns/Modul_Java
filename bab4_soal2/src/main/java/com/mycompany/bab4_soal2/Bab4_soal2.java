027ea2951846dc675b2403a85482b344191fd385
drianns <145427873+drianns@users.noreply.github.com>
2024-07-30 22:56:40 +0700

    Add files via upload

---
+++ bab4_soal2/src/main/java/com/mycompany/bab4_soal2/Bab4_soal2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4_soal2;
+
+/**
+ *
+ * @author zackg
+ */
+public class Bab4_soal2 {
+
+    public static void main(String[] args) {
+        int x = 6;
+        int w = 5;
+        int y = 8 - x++;
+        int z = 8 - ++w;
+        
+        x++;
+        ++w;
+        
+      
+        
+        System.out.println(w);
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+    }
+}
