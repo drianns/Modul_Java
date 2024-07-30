43a4fe978ce937a9461d7cd70618c108a7da9220
drianns <145427873+drianns@users.noreply.github.com>
2024-07-30 16:39:11 +0700

    Add files via upload

---
+++ bab2_soal2/src/main/java/com/mycompany/bab2_soal2/Bab2_soal2.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2_soal2;
+
+/**
+ *
+ * @author zackg
+ */
+public class Bab2_soal2 {
+    
+    public static String Makanan;
+    
+    public static final String Minuman = "Jus Jambu";
+ 
+ public static void main(String[] args) {
+     Makanan = "Ayam Geprek ";
+     System.out.println("Makanan Favorit Saya : " + Makanan);
+     
+     System.out.println("Minuman Favorit Saya : " + Minuman);
+ }
+}
