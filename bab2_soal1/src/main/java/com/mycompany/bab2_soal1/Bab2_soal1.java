89fc6fee51e5bf1097ff76bbf42d96bd56ad9dcd
drianns <145427873+drianns@users.noreply.github.com>
2024-07-30 16:38:54 +0700

    Add files via upload

---
+++ bab2_soal1/src/main/java/com/mycompany/bab2_soal1/Bab2_soal1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab2_soal1;
+
+/**
+ *
+ * @author zackg
+ */
+public class Bab2_soal1 {
+    
+    public static void main(String[] args) {
+      int kamu=50;
+      float aku=kamu;
+      System.out.println(kamu);
+      System.out.println(aku); 
+    }
+}
