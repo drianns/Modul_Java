231cf953e2e2e6802f4bfdc5d171a414a64aa735
drianns <145427873+drianns@users.noreply.github.com>
2024-07-30 21:58:15 +0700

    Add files via upload

---
+++ bab3_soal2/src/main/java/com/mycompany/bab3_soal2/Bab3_soal2.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab3_soal2;
+
+/**
+ *
+ * @author zackg
+ */
+public class Bab3_soal2 {
+
+    public static void main(String[] args) {
+        
+        int NIM = 2110117000;
+        String NamaMahasiswa = "Andika Jaya Utama";
+        String Kelas = "012SFE020";
+        double Usia = 19.5;
+        double TinggiBadan = 1.75;
+        
+        System.out.println("NIM : " +NIM);
+        System.out.println("Nama Mahasiswa : "+NamaMahasiswa);
+        System.out.println("Kelas : " + Kelas);
+        System.out.println("Usia : " + Usia);
+        System.out.println("Tinggi Badan : " +TinggiBadan);
+    }
+}
