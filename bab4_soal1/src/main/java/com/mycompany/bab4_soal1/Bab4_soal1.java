f6e12fab3593e9f51e2fe5c18b0e7addebbebe56
drianns <145427873+drianns@users.noreply.github.com>
2024-07-30 22:56:11 +0700

    Add files via upload

---
+++ bab4_soal1/src/main/java/com/mycompany/bab4_soal1/Bab4_soal1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab4_soal1;
+
+/**
+ *
+ * @author zackg
+ */
+public class Bab4_soal1 {
+
+    public static void main(String[] args) {
+        
+        int angka1 = 10;
+        int angka2 = 3;
+        
+        System.out.println("Hasil Pertambahan A+B = "+ (angka1+angka2) );
+        System.out.println("Hasil Pengurangan A-B = "+ (angka1-angka2) );
+        System.out.println("Hasil Pengurangan AXB = "+ (angka1*angka2) );
+        System.out.println("Hasil Pembagian A/B = "+ ( (float) angka1/angka2) );
+    }
+}
