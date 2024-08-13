70385f459cbcf5a8b3179b9d41f418f44f0039e1
drianns <145427873+drianns@users.noreply.github.com>
2024-08-13 10:12:36 +0700

    Add files via upload

---
+++ bab7_soal1/src/main/java/com/mycompany/bab7_soal1/Bab7_soal1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab7_soal1;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab7_soal1 {
+
+    public static void main(String[] args) {
+ int day = 2;
+ switch (day) {
+     case 1 :
+         System.out.println("Sabtu");
+         break;
+     case 2 :
+         System.out.println("Minggu");
+         break;
+ }
+}
+}
